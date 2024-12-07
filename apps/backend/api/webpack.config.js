93a02218ca1b991bf565b7733a290b2474a28ed6
m0r9h3us <joha.krauss@googlemail.com>
2024-12-07 16:12:31 +0100

    rename backend app to api

--- apps/backend/api/webpack.config.js
+++ apps/backend/api/webpack.config.js
@@ -3,7 +3,7 @@ const { join } = require('path');
 
 module.exports = {
     output: {
-        path: join(__dirname, '../../../dist/apps/backend/api-gateway')
+        path: join(__dirname, '../../../dist/apps/backend/api')
     },
     plugins: [
         new NxAppWebpackPlugin({
